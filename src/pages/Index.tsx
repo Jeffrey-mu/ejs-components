c209023c2f904f4b86db6205065b4b13b2dc9131
jeffrey-mu <1115378579@qq.com>
2024-03-04 09:03:33 +0800

    feat: 添加分类

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { api, header as headers } from "@/lib/constant";
 import CodeDemo, { CompDataType } from "@/components/CodeDemo";
+import { Tabs, Tab, Card, CardBody, Spinner } from "@nextui-org/react";
 export default function App() {
   const [list, setList] = useState<CompDataType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const tabs = ['card', 'footer']
   useEffect(() => {
     fetch(api + "/componentsList", {
       method: "get",
@@ -24,9 +27,28 @@ export default function App() {
 
   return (
     <div>
-      {list.map((item) => {
-        return <CodeDemo compData={item} />;
-      })}
+      <div className="flex w-full flex-col">
+        <Tabs aria-label="Options" onSelectionChange={() => {
+          setLoading(true)
+          setTimeout(() => {
+            setLoading(false)
+          }, 200)
+        }}>
+          {tabs.map((type) => {
+            return <Tab key={type} title={type}>
+              <Card>
+                <CardBody>
+                  {
+                    loading ? <Spinner /> : list.filter(item => item.info.type === type).map((item) =>
+                      <CodeDemo compData={item} />)
+                  }
+                </CardBody>
+              </Card>
+            </Tab>
+          })}
+        </Tabs>
+      </div>
+
     </div>
   );
 }
