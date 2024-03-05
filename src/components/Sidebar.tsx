ab4548e2dfbd799a097beea43c5deb707012d81d
jeffrey-mu <1115378579@qq.com>
2024-03-05 19:43:37 +0800

    wip: 优化代码

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,67 @@
+import ca from "clsx";
+import ArrowDown from "@/components/svg/ArrowDown";
+import { useLocation } from "react-use";
+import { tabs } from "@/pages/Index";
+import { CompDataType } from "./CodeDemo";
+export default function App({
+  change,
+  list,
+  active,
+}: {
+  change: (value: string) => void;
+  list: CompDataType[];
+  active: string;
+}) {
+  const state = useLocation();
+
+  function handelMenu(value: string) {
+    change(value);
+  }
+  return (
+    <div className="sticky top-[89px]">
+      <ul className="h-full text-foreground box-border bg-content1 outline-none shadow-medium rounded-large transition-transform-background flex w-full flex-col p-3 CodeDemo mb-3 gap-3 text-gray-700">
+        <li className="text-xl font-bold">Components</li>
+        {tabs.map((item) => {
+          return (
+            <>
+              <li onClick={handelMenu.bind(null, item)} key={item}>
+                <span
+                  className={ca(
+                    `${item === active ? "text-orange-600" : ""} w-full cursor-pointer pl-4 flex text-xl hover:text-orange-600 ml-3 items-center justify-between pr-5`,
+                  )}
+                >
+                  {item}
+                  <ArrowDown />
+                </span>
+
+                <ul className={ca(`mt-2`)}>
+                  {list
+                    .filter((item_list) => item_list.info.type === item)
+                    .map((item) => (
+                      <li
+                        className={ca(
+                          `border-l-1 ml-6 h-10  flex cursor-pointer`,
+                        )}
+                      >
+                        <a
+                          className={ca(
+                            `border-l-1 h-6 pl-6 flex border-transparent hover:text-orange-600 cursor-pointer w-full hover:border-orange-500 ml-[-1px]`,
+                            state.hash?.slice(1) === item.info.name
+                              ? "text-orange-600 border-orange-500"
+                              : "",
+                          )}
+                          href={`#${item.info.name}`}
+                        >
+                          {item.info.name}
+                        </a>
+                      </li>
+                    ))}
+                </ul>
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,14 @@
 import { useState, useEffect } from "react";
 import { api, header as headers } from "@/lib/constant";
 import CodeDemo, { CompDataType } from "@/components/CodeDemo";
-import { useLocation } from "react-use";
-import ca from "clsx";
-import ArrowDown from "@/components/svg/ArrowDown";
+import Sidebar from "@/components/Sidebar";
+
+export const tabs = ["card", "footer"];
 export default function App() {
-  const state = useLocation();
   const [list, setList] = useState<CompDataType[]>([]);
-  const tabs = ["card", "footer"];
   const [active, setActive] = useState(tabs[0]);
 
   useEffect(() => {
-    console.log(state, "state");
     fetch(api + "/componentsList", {
       method: "get",
       headers,
@@ -35,51 +32,7 @@ export default function App() {
   return (
     <div className="flex gap-5">
       <div className="w-[200px] ">
-        <div className="sticky top-[89px]">
-          <ul className="h-full text-foreground box-border bg-content1 outline-none shadow-medium rounded-large transition-transform-background flex w-full flex-col p-3 CodeDemo mb-3 gap-3">
-            <li className="text-xl font-bold">Components</li>
-            {tabs.map((item) => {
-              return (
-                <>
-                  <li onClick={handelMenu.bind(null, item)} key={item}>
-                    <span
-                      className={ca(
-                        `${item === active ? "text-orange-600" : ""} w-full cursor-pointer pl-4 flex text-xl hover:text-orange-600 ml-3 items-center justify-between pr-5`,
-                      )}
-                    >
-                      {item}
-                      <ArrowDown />
-                    </span>
-
-                    <ul className={ca(`mt-2`)}>
-                      {list
-                        .filter((item_list) => item_list.info.type === item)
-                        .map((item) => (
-                          <li
-                            className={ca(
-                              `border-l-1 ml-6 h-10  flex cursor-pointer `,
-                            )}
-                          >
-                            <a
-                              className={ca(
-                                `border-l-1 h-6 pl-6 flex border-transparent hover:text-orange-600 cursor-pointer w-full hover:border-orange-500`,
-                                state.hash?.slice(1) === item.info.name
-                                  ? "text-orange-600 border-orange-500"
-                                  : "",
-                              )}
-                              href={`#${item.info.name}`}
-                            >
-                              {item.info.name}
-                            </a>
-                          </li>
-                        ))}
-                    </ul>
-                  </li>
-                </>
-              );
-            })}
-          </ul>
-        </div>
+        <Sidebar change={handelMenu} list={list} active={active} />
       </div>
       <div className="flex flex-col w-[80%]  pb-80">
         {list
