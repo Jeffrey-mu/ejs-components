ee9ef196ede1b02069755a65916dbf9c17df9949
jeffrey-mu <1115378579@qq.com>
2024-03-06 11:23:10 +0800

    style: 修改字体颜色

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,7 +19,7 @@ export default function App({
   }
   return (
     <div className="sticky top-[89px]">
-      <ul className="h-full text-foreground box-border bg-content1 outline-none shadow-medium rounded-large transition-transform-background flex w-full flex-col p-3 CodeDemo mb-3 gap-3 text-gray-700 dark:text-gray-200">
+      <ul className="h-full box-border bg-content1 outline-none shadow-medium rounded-large transition-transform-background flex w-full flex-col p-3 CodeDemo mb-3 gap-3 text-yellow-800">
         <li className="text-xl font-bold">Components</li>
         {tabs.map((item) => {
           return (
