e03243e15db56bb492a8de2be2ce9e99fec09774
Jeffrey <1115378579@qq.com>
2024-03-11 12:15:19 +0800

    chore: 添加usehooks

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import ArrowDown from "@/components/svg/ArrowDown";
 import { useLocation } from "react-use";
 import { tabs } from "@/pages/Index";
 import { CompDataType } from "./CodeDemo";
+import { useWindowScroll } from "@uidotdev/usehooks";
 export default function App({
   change,
   list,
@@ -13,9 +14,10 @@ export default function App({
   active: string;
 }) {
   const state = useLocation();
-
+  const [, scrollTo] = useWindowScroll();
   function handelMenu(value: string) {
     change(value);
+    scrollTo({ left: 0, top: 0, behavior: "smooth" });
   }
   return (
     <div className="sticky top-[89px]">
@@ -31,32 +33,39 @@ export default function App({
                   )}
                 >
                   {item}
-                  <ArrowDown />
+                  <ArrowDown rotate={active === item ? "rotate-90" : ""} />
                 </span>
-
-                <ul className={ca(`mt-2`)}>
-                  {list
-                    .filter((item_list) => item_list.info.type === item)
-                    .map((item) => (
-                      <li
-                        className={ca(
-                          `border-l-1 ml-6 h-10  flex cursor-pointer`,
-                        )}
-                      >
-                        <a
+                {
+                  <ul
+                    className={ca(
+                      `mt-2 transform transition-all duration-500 ease-in-out ${
+                        active === item ? "block" : "hidden"
+                      }`,
+                    )}
+                  >
+                    {list
+                      .filter((item_list) => item_list.info.type === item)
+                      .map((item) => (
+                        <li
                           className={ca(
-                            `border-l-1 h-6 pl-6 flex border-transparent hover:text-orange-600 cursor-pointer w-full hover:border-orange-500 ml-[-1px]`,
-                            state.hash?.slice(1) === item.info.name
-                              ? "text-orange-600 border-orange-500"
-                              : "",
+                            `border-l-1 ml-6 h-10  flex cursor-pointer `,
                           )}
-                          href={`#${item.info.name}`}
                         >
-                          {item.info.name}
-                        </a>
-                      </li>
-                    ))}
-                </ul>
+                          <a
+                            className={ca(
+                              `border-l-1 h-6 pl-6 flex border-transparent hover:text-orange-600 cursor-pointer w-full hover:border-orange-500 ml-[-1px]`,
+                              state.hash?.slice(1) === item.info.name
+                                ? "text-orange-600 border-orange-500"
+                                : "",
+                            )}
+                            href={`#${item.info.name}`}
+                          >
+                            {item.info.name}
+                          </a>
+                        </li>
+                      ))}
+                  </ul>
+                }
               </li>
             </>
           );
--- src/components/svg/ArrowDown.tsx
+++ src/components/svg/ArrowDown.tsx
@@ -1,10 +1,13 @@
-export default function Svg() {
+export default function Svg({ rotate = "" }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
       viewBox="0 0 24 24"
+      className={
+        rotate ? "transform transition duration-500 ease-in-out " + rotate : ""
+      }
     >
       <path
         fill="none"
