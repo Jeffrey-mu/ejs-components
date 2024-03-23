e7f5a200abec33450159e7f32da4f00755f13fd7
Jeffrey <1115378579@qq.com>
2024-03-23 20:46:17 +0800

    feat: 添加svg

---
+++ src/components/svg/Pad.tsx
@@ -0,0 +1,8 @@
+export default () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 48 48">
+    <g fill="none" stroke="currentColor" stroke-width="4">
+      <rect width="38" height="30" x="5" y="10" rx="2" />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M11 27v-4" />
+    </g>
+  </svg>
+)
