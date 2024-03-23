9adb3cdd79e9f2b55d05ee8a0097ac0447876eb1
Jeffrey <1115378579@qq.com>
2024-03-23 21:32:39 +0800

    chore: 删除footer多余内容

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,87 +1,9 @@
-import Logo from './svg/Logo'
-import { appConfig } from '@/lib/constant'
-
 export default function Footer() {
   return (
     <>
       <footer className=" dark:text-white text-gray-900 border-t-1">
         <div className="mx-auto w-full p-4 py-6 lg:py-8  max_width">
-          <div className="md:flex md:justify-between">
-            <div className="mb-6 md:mb-0">
-              <a
-                href="https://flowbite.com/"
-                className="flex gap-2 items-center "
-              >
-                <Logo />
-                <span className="self-center text-2xl text-orange-600 font-semibold whitespace-nowrap className">
-                  {appConfig.siteName}
-                </span>
-              </a>
-            </div>
-            <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-                <h2 className="mb-6 text-sm font-semibold  uppercase className">
-                  Resources UI
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="https://flowbite.com/" className="hover:underline">
-                      Flowbite
-                    </a>
-                  </li>
 
-                  <li className="mb-4">
-                    <a href="https://nextui.org/" className="hover:underline">
-                      Nextui
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://tailwindcss.com/"
-                      className="hover:underline"
-                    >
-                      Tailwind CSS
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold uppercase className">
-                  Structure
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="https://react.dev/" className="hover:underline">
-                      React
-                    </a>
-                  </li>
-                  <li className="mb-4">
-                    <a href="https://vitejs.dev/" className="hover:underline">
-                      Vite
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold uppercase className">
-                  Legal
-                </h2>
-                <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                  <li className="mb-4">
-                    <a href="#" className="hover:underline">
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="hover:underline">
-                      Terms &amp; Conditions
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               © 2023
