3c861f6ea2f4a6c42bbeddf0b3ccdb76d6db5a53
jeffrey-mu <1115378579@qq.com>
2024-03-23 15:37:10 +0800

    feat: 添加代码编辑器

--- src/components/CodeDemo/RenderHtml.tsx
+++ src/components/CodeDemo/RenderHtml.tsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import clsx from 'clsx'
+import AceEditor from 'react-ace'
+import { useDebounce } from 'react-use'
 import Pc from '@/components/svg/Pc'
 import Mobile from '@/components/svg/Mobile'
-
+import EditCode from '@/components/svg/EditCode'
+import 'ace-builds/src-noconflict/mode-jsx'// jsx模式的包
+import 'ace-builds/src-noconflict/theme-monokai'// monokai的主题样式
+import 'ace-builds/src-noconflict/ext-language_tools'
+// 代码联想
 export default function App({ html, mode }: { html: string, mode?: boolean }) {
   const [width, setWidth] = useState(400)
   const [height, setHeight] = useState(550)
+  const [innerHtml, setInnerHtml] = useState(html)
+  const [showEdit, setShowEdit] = useState(false)
+  const [, cancel] = useDebounce(
+    () => {
+      console.log('useDebounce change')
+      setInnerHtml(innerHtml)
+    },
+    500,
+    [innerHtml],
+  )
   // 模拟响应式
   const screen = [
     {
@@ -47,11 +63,19 @@ export default function App({ html, mode }: { html: string, mode?: boolean }) {
   return mode
     ? (
       <>
-        <div className="flex gap-3 justify-end items-center">
+        <div className="flex gap-3 justify-end items-center mb-2">
+          <span
+            onClick={setShowEdit.bind(null, !showEdit)}
+            className={clsx(
+            `${showEdit ? 'text-orange-400' : ''} bg-slate-50 p-1 cursor-pointer icon-hover`,
+            )}
+          >
+            <EditCode />
+          </span>
           {screen.map(item => (
             <span
               className={clsx(
-              `cursor-pointer ${width === item.width ? 'text-orange-500' : ''} icon-hover `,
+                `cursor-pointer bg-slate-50 p-1  ${width === item.width ? 'text-orange-400' : ''} icon-hover `,
               )}
               onClick={switchDevice.bind(null, item.type)}
             >
@@ -59,13 +83,46 @@ export default function App({ html, mode }: { html: string, mode?: boolean }) {
             </span>
           ))}
         </div>
+        {
+          showEdit ? (
+            <div className="p-2 border-2">
+              <AceEditor
+                mode="jsx"
+                theme="monokai"
+                name="app_code_editor"
+                fontSize={14}
+                showPrintMargin
+                height="200px"
+                width="100%"
+                showGutter
+                value={innerHtml}
+                onChange={(value) => {
+                  console.log('change')
+                  setInnerHtml(value)
+                }}
+                wrapEnabled
+                highlightActiveLine // 突出活动线
+                enableSnippets // 启用代码段
+                setOptions={{
+                  enableBasicAutocompletion: true, // 启用基本自动完成功能
+                  enableLiveAutocompletion: true, // 启用实时自动完成功能 （比如：智能代码提示）
+                  enableSnippets: true, // 启用代码段
+                  showLineNumbers: true,
+                  tabSize: 2,
+                }}
+                annotations={[{ row: 0, column: 2, type: 'error', text: 'Some error.' }]}
+              />
+            </div>
+          ) : <></>
+        }
+
         <div className="flex justify-center">
           <iframe
             className={clsx(
-            `w-[${width}px] h-[${height}px] border-dashed border-2 p-3 overflow-auto hover:border-orange-600 hover:bg-slate-50`,
+              `w-[${width}px] h-[${height}px] border-dashed border-2 p-3 overflow-auto hover:border-orange-400 hover:bg-slate-50`,
             )}
             srcDoc={renderHtml(
-              html
+              innerHtml
                 .replace('data-src', 'src')
                 .replace('<a ', '<a onClick="event.preventDefault();" '),
             )}
