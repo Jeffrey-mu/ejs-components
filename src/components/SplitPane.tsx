55cba5299337dc961fb47b2831551c96058862b0
Jeffrey <1115378579@qq.com>
2024-04-06 17:54:23 +0800

    chore: 禁止使用

--- src/components/SplitPane.tsx
+++ src/components/SplitPane.tsx
@@ -1,33 +1,34 @@
-import type { ReactNode } from 'react'
-import { useState } from 'react'
-import SplitPane, { Pane } from 'split-pane-react'
+// import type { ReactNode } from 'react'
+// import { useState } from 'react'
+// import SplitPane, { Pane } from 'split-pane-react'
 import 'split-pane-react/esm/themes/default.css'
 
-interface Props {
-  rightSlot: ReactNode
-  leftSlot: ReactNode
-}
-export default function App(props: Props) {
-  const [sizes, setSizes] = useState([500, 'auto'])
-  const { rightSlot, leftSlot } = props
-  return (
-    <div style={{ height: 500 }}>
-      <SplitPane
-        split="horizontal"
-        sizes={sizes}
-        onChange={setSizes}
-      >
-        <Pane minSize={200}>
-          <div>
-            {leftSlot}
-          </div>
-        </Pane>
-        <Pane minSize={200}>
-          <div>
-            {rightSlot}
-          </div>
-        </Pane>
-      </SplitPane>
-    </div>
-  )
+// interface Props {
+//   rightSlot: ReactNode
+//   leftSlot: ReactNode
+// }
+export default function App() {
+  // const [sizes, setSizes] = useState([500, 'auto'])
+  // const { rightSlot, leftSlot } = props
+  return <></>
+  // return (
+  //   <div style={{ height: 500 }}>
+  //     <SplitPane
+  //       split="horizontal"
+  //       sizes={sizes}
+  //       onChange={setSizes}
+  //     >
+  //       <Pane minSize={200}>
+  //         <div>
+  //           {leftSlot}
+  //         </div>
+  //       </Pane>
+  //       <Pane minSize={200}>
+  //         <div>
+  //           {rightSlot}
+  //         </div>
+  //       </Pane>
+  //     </SplitPane>
+  //   </div>
+  // )
 };
